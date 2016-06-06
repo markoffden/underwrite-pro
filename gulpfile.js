a317fd74f4760d80ec049a6faa977d04e63656d1
Denys Markov <denys.markov@n3wnormal.com>
2016-06-06 21:13:42 +0300

    add app folder, setup livereload and builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,56 @@
-// Require Dependencies
+// require dependencies
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    browserify = require('gulp-browserify'),
+    connect = require('gulp-connect'),
     sass = require('gulp-sass'),
-    concat = require('gulp-concat');
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename');
 
-// Source Variables
+// declare variables
+htmlSrc = ['app/*.html'];
 sassSrc = ['assets/sass/style.scss'];
+jsSrc = ['assets/js/script.js'];
 
-// Create Tasks
+// create build tasks
+gulp.task('html', function () {
+    gulp.src(htmlSrc)
+        .pipe(connect.reload());
+});
+
 gulp.task('sass', function () {
-    return gulp.src('assets/sass/style.scss')
+    return gulp.src(sassSrc)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('builds/development/css'));
+        .pipe(autoprefixer({browsers: ['last 2 versions']}))
+        .pipe(cleanCSS())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/css'))
+        .pipe(connect.reload());
 });
 
-gulp.task('watch', function () {
-    gulp.watch('assets/sass/**/*.scss', ['sass']);
-    gutil.log('Gulp is watching...');
+gulp.task('js', function () {
+    gulp.src(jsSrc)
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/js'))
+        .pipe(connect.reload());
 });
+
+// create server to watch changes
+gulp.task('connect', function () {
+    connect.server({
+        root: 'app',
+        livereload: true
+    });
+});
+
+// watcher
+gulp.task('watch', function () {
+    gulp.watch(htmlSrc, ['html']);
+    gulp.watch('assets/sass/**/*.scss', ['sass']);
+    gulp.watch(jsSrc, ['js']);
+});
+
+// default task
+gulp.task('default', ['connect', 'html', 'sass', 'js', 'watch']);
