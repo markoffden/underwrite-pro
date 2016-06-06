6de04677a57a4e8010684232fc7d48ded7b4e47b
Denys Markov <denys.markov@n3wnormal.com>
2016-06-06 14:22:03 +0300

    added bootstrap dashboard

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
+// Require Dependencies
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    browserify = require('gulp-browserify'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat');
 
-gulp.task('log', function () {
-    gutil.log('Gulp is Awesome');
+// Source Variables
+sassSrc = ['assets/sass/style.scss'];
+
+// Create Tasks
+gulp.task('sass', function () {
+    return gulp.src('assets/sass/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('builds/development/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('assets/sass/**/*.scss', ['sass']);
+    gutil.log('Gulp is watching...');
 });
